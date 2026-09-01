4e773e543144d369361309e6418eaa427151bbdf
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-09-02 03:25:41 +0530

    Create post.sql

---
+++ post.sql
@@ -0,0 +1,8 @@
+-- Write an SQL statement to create a new table named posts, which should contain the following columns:
+CREATE TABLE posts (
+  id INTEGER,
+  image_url TEXT,
+  description TEXT,
+  author_id INTEGER,
+  is_sponsored BOOLEAN
+);
