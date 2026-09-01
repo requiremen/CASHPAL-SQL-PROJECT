f2470ea6e190220f48bc887bc91ec51ac942f250
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-09-02 03:29:31 +0530

    Update posts table schema
    Renamed author_id to poster_id, added is_edited column, and dropped is_sponsored column in posts table.

--- post.sql
+++ post.sql
@@ -6,3 +6,19 @@ CREATE TABLE posts (
   author_id INTEGER,
   is_sponsored BOOLEAN
 );
+
+-- Write an up migration for the posts table that achieves the following:
+
+--The author_id column should be renamed to poster_id.
+
+--Add a new column named is_edited with a BOOLEAN type.
+--DROP the is_sponsored column.
+
+ALTER TABLE posts
+RENAME author_id to poster_id;
+
+ALTER TABLE posts
+ADD COLUMN is_edited BOOLEAN;
+
+ALTER TABLE posts
+DROP COLUMN is_sponsored;
