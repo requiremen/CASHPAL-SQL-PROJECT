761d447a2e982bbfef903ef3784c0d50d8d3d6d8
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-09-02 02:53:17 +0530

    Create PEOPLE.sql

---
+++ PEOPLE.sql
@@ -0,0 +1,9 @@
+-- Let's begin building a table for the CashPal database! Create the people table with the following fields:
+CREATE TABLE people 
+  (id INTEGER, 
+  tag TEXT, 
+  name TEXT, 
+  age INTEGER, 
+  balance REAL, 
+  is_admin BOOLEAN);
+--
